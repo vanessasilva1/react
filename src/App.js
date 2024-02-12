a92850011f8f93389f6df1c199a4330ae4c3c50b
Vanessa <vanessa35lv@gmail.com>
2024-02-12 16:17:50 -0300

    Props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
+
+  const nome = "Maria"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelloWorld />
+      <SayMyName nome="Vanessa" />
+      <SayMyName nome="João" />
+      <SayMyName nome="Rosa" />
+      <SayMyName nome={nome} />
+      <Pessoa 
+        nome="Eduardo" 
+        idade="25" 
+        profissao="Arquiteto"
+        foto="https://via.placeholder.com/150"
+      />
     </div>
   );
+
 }
 
 export default App;
---
+++ src/components/SayMyName.js
@@ -0,0 +1,9 @@
+function SayMyName(props) {
+    return (
+        <div>
+            <p>Tudo bem {props.nome}, está tudo ok?</p>
+        </div>
+    )
+}
+
+export default SayMyName;
